aeeea8667196d6c65ea16915cc9b4c6d2f4e3865
hdragon324 <blackneo11@gmail.com>
2025-03-19 16:45:29 +0900

    week3

--- utils/get_text.py
+++ utils/get_text.py
@@ -1,9 +1,22 @@
 from langchain.document_loaders import PyPDFLoader
 
+import cv2
 import re
-import olefile
 import zlib
 import struct
+import olefile
+import pytesseract
+
+import numpy as np
+import platform
+from PIL import ImageFont, ImageDraw, Image
+from matplotlib import pyplot as plt
+ 
+import uuid
+import json
+import time
+import cv2
+import requests
 
 def get_pdf_text(filename):
 
@@ -85,3 +98,91 @@ def get_hwp_text(filename):
         text = re.sub(r'[^\x00-\x7F\uAC00-\uD7AF]', '', text)
         
     return text  # 최종적으로 추출된 텍스트 반환
+
+# def get_image_text(filename):
+#     # Tesseract-OCR 경로 설정
+#     pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\every\\Desktop\\Project\\team_project\\SeSAC_Final\\AI-for-Creating-Business-Plan-Drafts\\OCR\\tesseract.exe"
+
+
+#     # 이미지 로드
+#     img = cv2.imread(filename)
+#     dst = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # tesseract에서는 RGB로 넣어야 하기 때문에 변환
+
+#     # 이미지에서 텍스트 추출
+#     text = pytesseract.image_to_string(dst, lang='kor+eng')
+    
+#     return text
+
+def put_text(image, text, x, y, color=(0, 255, 0), font_size=22):
+    '''한글 깨짐을 방지하기 위한 함수'''
+    if type(image) == np.ndarray:
+        color_coverted = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(color_coverted)
+ 
+    if platform.system() == 'Darwin':
+        font = 'AppleGothic.ttf'
+    elif platform.system() == 'Windows':
+        font = 'malgun.ttf'
+        
+    image_font = ImageFont.truetype(font, font_size)
+    font = ImageFont.load_default()
+    draw = ImageDraw.Draw(image)
+ 
+    draw.text((x, y), text, font=image_font, fill=color)
+    
+    numpy_image = np.array(image)
+    opencv_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+ 
+    return opencv_image
+
+def get_image_text(path):
+    api_url = 'https://dqfp3rdhcw.apigw.ntruss.com/custom/v1/39549/6ec703e8b494511fd50f11c0df474b47bdc8717eb906e3b6c5bab61e56621fba/general' # NAVER CLOVA invoke url 입력
+    secret_key = 'U290ZXdjRFpBWVhPcFFGVWplRVRJT0RBQk9FS1VBb0k=' # NAVER CLOVA key 입력
+
+    files = [('file', open(path,'rb'))]
+
+    request_json = {'images': [{'format': 'jpg',
+                                'name': 'demo'
+                               }],
+                    'requestId': str(uuid.uuid4()),
+                    'version': 'V2',
+                    'timestamp': int(round(time.time() * 1000))
+                   }
+ 
+    payload = {'message': json.dumps(request_json).encode('UTF-8')}
+    
+    headers = {
+    'X-OCR-SECRET': secret_key,
+    }
+    
+    response = requests.request("POST", api_url, headers=headers, data=payload, files=files)
+    result = response.json()
+
+    img = cv2.imread(path)
+    roi_img = img.copy()
+
+    # 추출된 텍스트를 저장할 리스트
+    extracted_texts = []
+
+    for field in result['images'][0]['fields']:
+        text = field['inferText']
+        extracted_texts.append(text)  # 리스트에 저장
+
+        vertices_list = field['boundingPoly']['vertices']
+        pts = [tuple(vertice.values()) for vertice in vertices_list]
+        topLeft = [int(_) for _ in pts[0]]
+        topRight = [int(_) for _ in pts[1]]
+        bottomRight = [int(_) for _ in pts[2]]
+        bottomLeft = [int(_) for _ in pts[3]]
+
+        cv2.line(roi_img, topLeft, topRight, (0,255,0), 2)
+        cv2.line(roi_img, topRight, bottomRight, (0,255,0), 2)
+        cv2.line(roi_img, bottomRight, bottomLeft, (0,255,0), 2)
+        cv2.line(roi_img, bottomLeft, topLeft, (0,255,0), 2)
+
+        roi_img = put_text(roi_img, text, topLeft[0], topLeft[1] - 10, font_size=30)
+  
+    # 각 단어를 공백 구분자로 구분
+    result = " ".join(extracted_texts)
+
+    return result
