a2b14de856164b15b169f68c710b5b9cec3e67de
joseo22 <joseo22222@gmail.com>
2025-03-19 15:51:22 +0900

    Initial Commit

---
+++ youth_startup.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def display_youth_startup():
+    st.subheader("청년창업사관학교")
+    st.write("청년창업사관학교에 맞춘 사업계획서를 작성합니다.")
+    # 추가적으로 필요한 입력 양식이나 내용을 여기에 작성합니다.
+    st.text_input("사업 아이디어 입력")
